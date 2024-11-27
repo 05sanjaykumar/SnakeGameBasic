44539cffa658a7bd75379cd39ecf490defcf1c2a
SanjayKumar <0508sanjaykumar@gmail.com>
2024-11-27 06:43:26 +0530

    Added Base Routing

---
+++ src/Components/Game.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Game:React.FC = () => {
+  return (
+    <div>
+      hi this is Game
+    </div>
+  )
+}
+
+export default Game
---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home:React.FC = () => {
+  return (
+    <div>
+      hi this is home
+    </div>
+  )
+}
+
+export default Home
