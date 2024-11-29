8cb95fbab7612d633290274bc1d80277638c893c
SanjayKumar <0508sanjaykumar@gmail.com>
2024-11-30 00:18:07 +0530

    Added rows and cols

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Game.css';
 
+const ROWS = 25;
+const COLS = 25;
+const INITIAL_SNAKE = [{ row: 12, col: 12 }];
+const INITIAL_DIRECTION = 'RIGHT';
 
 
 const Game:React.FC = () => {
