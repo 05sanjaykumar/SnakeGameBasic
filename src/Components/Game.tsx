12b87b27b34b8d0584310b222c9d9fc3e83f4ac7
SanjayKumar <0508sanjaykumar@gmail.com>
2024-11-30 08:05:19 +0530

    Inital commit

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -39,6 +39,42 @@ const App: React.FC = () => {
   }, [direction])
   
 
+  const resetGame = () => {
+    setSnake([{ row: 10, col: 10 }]);
+    setDirection('RIGHT');
+    setFood(generateFood());
+    setGameOver(false);
+    setScore(0);
+  };
+
+
+  const moveSnake = ()=>{
+    const head = {...snake[0]};
+    const newSnake = [...snake];
+
+    if (direction === 'UP') head.row -= 1;
+    else if (direction === 'DOWN') head.row += 1;
+    else if (direction === 'LEFT') head.col -= 1;
+    else if (direction === 'RIGHT') head.col += 1;
+
+    head.row = (head.row + ROWS) % ROWS;
+    head.col = (head.col + COLS) % COLS;
+
+    if (checkCollision(newSnake, head)) {
+      setGameOver(true);
+      return;
+    }
+
+    
+  }
+
+  
+
+
+  function checkCollision(snake:{row:number,col:number}[], head:{row:number,col:number})
+  {
+    return snake.some((part)=>part.row === head.row && part.col === head.col)
+  }
   return (
     <div>
       Hello
